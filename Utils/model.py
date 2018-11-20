f1d017e9dd560bedd9e343cb65d921d162510148
Z-Codlin <552411868@qq.com>
2018-11-20 23:39:34 +0800

    2018-11-20 23:39 update

--- Utils/model.py
+++ Utils/model.py
@@ -30,4 +30,52 @@ def inference(images, batch_size, n_classes):
         pre_activation = tf.nn.bias_add(conv, biases)
         conv1 = tf.nn.relu(pre_activation, name= scope.name)
 
+#池化层1
+#3x3最大池化，步长strides为2，池化后执行lrn()操作，局部响应归一化，对训练有利。<lrn()是什么？>
     with tf.variable_scope('pooling1_lrn') as scope:
+        pool1 = tf.nn.max_pool(conv1, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME', name='pooling1')
+        norm1 = tf.nn.lrn(pool1, depth_radius=4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm1')
+
+#卷积层2
+#16个3x3的卷积核（16通道），padding='SAME'，激活函数relu
+    with tf.variable_scope('conv2') as scope:
+        weights = tf.Variable(tf.truncated_normal(shape=[3,3,64,16], stddev=0.1, dtype=tf.float32),
+                              name='weights', dtype=tf.float32)
+        biases = tf.Variable(tf.constant(value=0.1, dtype=tf.float32, shape=[16]),
+                             name='biases', dtype=tf.float32)
+        conv = tf.nn.conv2d(norm1, weights, strides=[1,1,1,1], padding='SAME')
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(pre_activation, name='conv2')
+
+#池化层2
+#3x3 最大池化
+    with tf.variable_scope('pool2_lrn') as scope:
+        pool2 = tf.nn.max_pool(conv2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME', name='pooling2')
+        norm2 = tf.nn.lrn(pool2, depth_radius=4, bias=1.0, alpha=0.001/9.0, beta=0.75, name='norm2')
+
+#全连接层3
+#128个神经元，将之前pool层的输出reshape成一行，激活函数relu()
+    with tf.variable_scope('local3') as scope:
+        reshape = tf.reshape(norm2, shape=[batch_size, -1])
+        dim = reshape.get_shape()[1].value
+        weights = tf.Variable(tf.truncated_normal(shape=[dim,128], stddev=0.005, dtype=tf.float32),
+                              name='weights', dtype=tf.float32)
+        biases = tf.Variable(tf.constant(value=0.1, dtype=tf.float32, shape=[128]),
+                             name='biases', dtype=tf.float32)
+        local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope._name)
+
+#全连接层4
+#128个神经元，激活函数relu()
+    with tf.variable_scope('local4') as scope:
+        weights = tf.Variable(tf.truncated_normal(shape=[128,128], stddev=0.005, dtype=tf.float32),
+                              name='weights', dtype=tf.float32)
+        biases = tf.Variable(tf.constant(value = 0.1, dtype = tf.float32, shape=[128]),
+                             name='biases', dtype=tf.float32)
+        local4 = tf.nn.relu(tf.matmul(local3, weights)+biases, name='local4')
+
+#dropout层
+#   with tf.variable_scope('dropout') as scope:
+#       drop_out = tf.nn.dropout(local4, 0.8)
+
+#Softmax回归层
+#将前面的FC层输出，做一个线性回归，计算出每一类的得分，在这里是两类，所以这个层输出的是两个得分。
